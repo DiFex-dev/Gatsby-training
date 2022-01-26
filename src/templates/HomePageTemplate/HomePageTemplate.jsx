4d0fc8432003aa79a13c580d2a89879737e6a353
DiFex-dev <derenok2002@gmail.com>
2022-01-26 18:27:57 +0300

    Header+hero+footer

--- src/templates/HomePageTemplate/HomePageTemplate.jsx
+++ src/templates/HomePageTemplate/HomePageTemplate.jsx
@@ -1,36 +1,45 @@
 import { withPrefix } from 'gatsby';
 import React from 'react';
-
-import { Divider } from '../../components/Divider';
+import HeadAdvImpact from '@economist/fabric-components/header/headadvimpact/HeadAdvImpact';
+import HeadImpact from '@economist/fabric-components/header/headimpact/HeadImpact';
+import UtilSocShare from '@economist/fabric-components/utility/utilsocshare/UtilSocShare';
+import FootEconSec from '@economist/fabric-components/footer/footeconsec/FootEconSec';
 import { Layout } from '../../components/Layout';
-import { Text } from '../../components/Text';
-import { Header } from '../../components/Text/Header';
-import { Subheader } from '../../components/Text/Subheader';
-import { Subtitle } from '../../components/Text/Subtitle';
-import { Title } from '../../components/Text/Title';
 import { Wrapper } from '../../components/Wrapper';
 import styles from './HomePageTemplate.styles';
 
-const HomePageTemplate = ({ pageContext: { PageData } }) => (
-  <Layout title={PageData.seo.title} description={PageData.seo.description} image={PageData.seo.image}>
-    <div css={styles}>
-      <Wrapper maxWidth={'1440px'} width={'100%'} mobileWidth={'90%'}>
-        <div className="template-container">
-          <Title text={PageData.hero.title} width={'70%'} />
-          <Text text={'Hello word'} maxWidth={'100px'} />
-          <Divider height={6} />
-          <Subtitle text={PageData.hero.subtitle} />
-          <Text text={PageData.text.first} />
-          <Divider height={'45px'} />
-          <Header text={'Header'} width={'90%'} maxWidth={'300px'} fontWeight={500} />
-          <Divider height={2} />
-          <Subheader text={'Subheader'} maxWidth={'200px'} fontWeight={'normal'} />
-          <Divider height={8} />
-          <img src={withPrefix(PageData.hero.backgroundImage)} alt="heroImage" />
-        </div>
-      </Wrapper>
-    </div>
-  </Layout>
-);
+const HomePageTemplate = ({ pageContext: { PageData } }) => {
+  const {hero,} = PageData;
+  const sponsorsList = [
+    {
+      sponsorLogo: hero.sponsorLogo,
+      sponsorName: 'Desktop Logo',
+      sponsorLink: 'https://www.economist.com/'
+    },
+  ];
+  return (
+    <Layout title={PageData.seo.title} description={PageData.seo.description} image={PageData.seo.image}>
+      <div css={styles}>
+        <Wrapper maxWidth={'1440px'} width={'100%'} mobileWidth={'90%'}>
+          <div className="template-container">
+            <div className="Hero">
+              <div className="title-container">
+                <UtilSocShare hasBorder={false} className="social"/>
+                <p className="Title">Gold Demand Trends:<br/>Q2 2021 </p>
+              </div>
+              <div className="Image">
+                <a href='#' className="Download-btn">
+                  Download report
+                  <img src={PageData.hero.buttonIcon} alt="buttonIcon" />
+                </a>
+                <img src={withPrefix(PageData.hero.backgroundImage)} alt="heroImage" />
+              </div>
+            </div>
+          </div>
+        </Wrapper>
+      </div>
+    </Layout>
+  );
+}
 
 export default HomePageTemplate;
